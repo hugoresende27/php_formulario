ff8a08429a42c00927817162b5b6fe0cb9ea8e0c
hugoresende27 <Hugoresende27@gmail.com>
2021-09-19 21:41:27 +0100

    dom  noite

--- mensagem.php
+++ mensagem.php
@@ -1,11 +1,26 @@
 <?php
 
-echo "Enviado do ficheiro PHP --- TESTE";
-//valores do form
-$nome =$_POST['nome'];
-$mail =$_POST['mail'];
-$tele =$_POST['tele'];
-$site =$_POST['site'];
-$msg =$_POST['msg'];
+$nome =htmlspecialchars($_POST['nome']);
+$mail =htmlspecialchars($_POST['mail']);
+$tele =htmlspecialchars($_POST['tele']);
+$site =htmlspecialchars($_POST['site']);
+$msg =htmlspecialchars($_POST['msg']);
+
+if (!empty ($mail) && !empty($msg) ) 
+{       //se mail e mensagem vazia
+   // if(filter_var($mail, FILTER_VALIDATE_mail)){
+        $receiver = "hugoresende27@gmail.com"; //enter that mail address where you want to receive all messages
+        $subject = "From: $nome <$mail>";
+        $body = "nome: $nome\nEmail: $mail\nTelemóvel: $tele\nWebsite: $site\n\nMensagem:\n$msg\n\nCumprimentos,\n$nome";
+        $sender = "From: $mail";
+        if(mail($receiver, $subject, $body, $sender)){
+        echo "Mensagem enviada";
+        }else{
+        echo "Sorry, failed to send your message!";
+        }
+    }else{
+        echo "Enter a valid mail address!";
+    }
+//}else{echo "mail and message field is required!";}
 
 ?>
--- scriptForm.js
+++ scriptForm.js
@@ -2,21 +2,8 @@ const form = document.querySelector("form"),
 statusTxt = form.querySelector(".btn-campo span");
 
 form.onsubmit = (e)=>{
-  /*  e.preventDefault();                     //evitar que envie
-    statusTxt.style.display="block";
-
-    let var1 = new XMLHttpRequest();         //criar um novo objeto xml
-    var1.open("POST","mensagem.php",true);  //envia o POST request para o ficheiro mensagem.php
-    var1.onload = ()=>{                     //depois de carregamento ajax
-        if (var1.readyState == 4 && var1.status==200){//ajax responde 200 e ready status 4 significa q não há erros
-            let resposta = var1.resposta;//guarda resposta ajax na var resposta
-            console.log(resposta);
-        }
-    }
-    var1.send();*/
-
  e.preventDefault();
-  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.color = "#bfbfbf";
   statusTxt.style.display = "block";
   statusTxt.innerText = "Sending your message...";
   form.classList.add("disabled");
